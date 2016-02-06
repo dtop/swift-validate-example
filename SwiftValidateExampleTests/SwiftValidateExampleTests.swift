24dd464031a8c8377c635af287d22cfe8ea9d8d5
Danilo Topalovic <danilo.topalovic@nerdsee.com>
2016-02-06 15:41:03 +0100

    Initial Commit

---
+++ SwiftValidateExampleTests/SwiftValidateExampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftValidateExampleTests.swift
+//  SwiftValidateExampleTests
+//
+//  Created by Danilo Topalovic on 06.02.16.
+//  Copyright © 2016 Danilo Topalovic. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftValidateExample
+
+class SwiftValidateExampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
